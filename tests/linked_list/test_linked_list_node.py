b55980836019d35c5ef6f4f98723ec81ba67dda3
phunkault <phunkault@gmail.com>
2024-01-19 16:36:10 +0400

    refactor(linked_list_node): rewrite tests for linked_list_node

--- tests/linked_list/test_linked_list_node.py
+++ tests/linked_list/test_linked_list_node.py
@@ -2,7 +2,7 @@ import pytest
 from src.linked_list.linked_list_node import LinkedListNode
 
 
-test_values = (1, "1", 1.1, True)
+test_values = (1, "1", 1.1, True, None, [1, 2, 3], {"1", "2", "3"}, {"key": 1})
 
 
 @pytest.mark.parametrize("test_value", test_values)
@@ -12,28 +12,6 @@ def test_linked_list_node_init_with_primitive_value_type(test_value):
 
     # Assert
     assert node.value == test_value
-    assert node.next is None
-
-
-def test_linked_list_node_init_with_dict_value_type():
-    # Arrange
-    test_dict_value = {"key": 1}
-
-    # Act
-    node = LinkedListNode(test_dict_value)
-
-    # Assert
-    assert node.value == test_dict_value
-    assert node.value["key"] == 1
-    assert node.next is None
-
-
-def test_linked_list_node_init_without_value():
-    # Act
-    node = LinkedListNode(None)
-
-    # Assert
-    assert node.value is None
     assert node.next is None
 
 
