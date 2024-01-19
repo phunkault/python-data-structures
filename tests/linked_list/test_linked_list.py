09a51abacdbeca6bdf5630fa931b033b7c84ded3
phunkault <phunkault@gmail.com>
2024-01-19 23:26:20 +0400

    fix(linked_list): add fixture get_test_linked_list

--- tests/linked_list/test_linked_list.py
+++ tests/linked_list/test_linked_list.py
@@ -1,7 +1,14 @@
-# import pytest
+import pytest
+
+
+@pytest.fixture
+def get_test_linked_list():
+    # return LinkedList()
+    pass
 
 
 def test_initial_state(linked_list):
-    assert linked_list.head is None
-    assert linked_list.tail is None
-    assert linked_list.size == 0
+    # assert linked_list.head is None
+    # assert linked_list.tail is None
+    # assert linked_list.size == 0
+    pass
