8b5fd7ed8af209327d45e8d07dfbd11d19cf4217
phunkault <phunkault@gmail.com>
2024-01-19 16:02:23 +0400

    fix(node): remove __eq__

--- src/linked_list/linked_list_node.py
+++ src/linked_list/linked_list_node.py
@@ -5,8 +5,3 @@ class LinkedListNode:
 
     def __str__(self) -> str:
         return f"LinkedListNode({self.value})"
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, LinkedListNode):
-            return self.value == other.value and self.next == other.next
-        return False
