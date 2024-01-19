e02049e319b6577417ff0f6ba0429c6fe9bd4d2d
phunkault <phunkault@gmail.com>
2024-01-20 01:11:44 +0400

    feat(linked_list): add append method of LinkedList class

--- src/linked_list/linked_list.py
+++ src/linked_list/linked_list.py
@@ -1,5 +1,25 @@
+from src.linked_list.linked_list_node import LinkedListNode
+
+
 class LinkedList:
-    def __init__(self):
+    def __init__(self, initial=None) -> None:
         self.head = None
         self.tail = None
         self.size = 0
+
+        if initial:
+            node = LinkedListNode(initial)
+            self.head = node
+            self.tail = node
+            self.size = 1
+
+    def append(self, value):
+        node = LinkedListNode(value)
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
+        return self
--- tests/linked_list/test_linked_list.py
+++ tests/linked_list/test_linked_list.py
@@ -2,12 +2,65 @@ import pytest
 from src.linked_list.linked_list import LinkedList
 
 
+# Arrange
 @pytest.fixture
-def get_test_linked_list():
+def empty_linked_list():
     return LinkedList()
 
 
-def test_initial_state(get_test_linked_list):
-    assert get_test_linked_list.head is None
-    assert get_test_linked_list.tail is None
-    assert get_test_linked_list.size == 0
+# Arrange
+@pytest.fixture
+def non_empty_linked_list():
+    return LinkedList(1)
+
+
+def test_initial_state(empty_linked_list):
+    # Assert
+    assert empty_linked_list.head is None
+
+    assert empty_linked_list.tail is None
+
+    assert empty_linked_list.size == 0
+
+
+def test_append_empty_list(empty_linked_list):
+    # Act
+    empty_linked_list.append(1)
+
+    # Assert
+    assert empty_linked_list.head.value == 1
+    assert empty_linked_list.head.next is None
+
+    assert empty_linked_list.tail.value == 1
+    assert empty_linked_list.tail.next is None
+
+    assert empty_linked_list.size == 1
+
+
+def test_append_call_chain(empty_linked_list):
+    # Act
+    empty_linked_list.append(1).append(2).append(3)
+
+    # Assert
+    assert empty_linked_list.head.value == 1
+    assert empty_linked_list.head.next.value == 2
+    assert empty_linked_list.head.next.next.value == 3
+
+    assert empty_linked_list.tail.value == 3
+    assert empty_linked_list.tail.next is None
+
+    assert empty_linked_list.size == 3
+
+
+def test_append_non_empty_list(non_empty_linked_list):
+    # Act
+    non_empty_linked_list.append(2)
+
+    # Assert
+    assert non_empty_linked_list.head.value == 1
+    assert non_empty_linked_list.head.next.value == 2
+
+    assert non_empty_linked_list.tail.value == 2
+    assert non_empty_linked_list.tail.next is None
+
+    assert non_empty_linked_list.size == 2
