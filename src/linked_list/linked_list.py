e10272940a6965611afbbaa29021de2ff4cdec0d
phunkault <phunkault@gmail.com>
2024-01-22 13:58:24 +0400

    refactor(linked_list): add type hints for LinkedList class

--- src/linked_list/linked_list.py
+++ src/linked_list/linked_list.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 from src.linked_list.linked_list_node import LinkedListNode
+from typing import Any
 
 
 class LinkedList:
-    def __init__(self, initial=None) -> None:
+    def __init__(self, initial: Any = None) -> None:
         self.head = None
         self.tail = None
         self.size = 0
@@ -14,7 +17,7 @@ class LinkedList:
             self.tail = node
             self.size = 1
 
-    def append(self, value):
+    def append(self, value: Any) -> LinkedList:
         node = LinkedListNode(value)
 
         if not self.head:
