2df5ea3acebb602614b88542bbfe3afa1f3036d1
phunkault <phunkault@gmail.com>
2024-01-22 13:54:17 +0400

    refactor(linked_list): add type hints for LinkedListNode class

--- src/linked_list/linked_list_node.py
+++ src/linked_list/linked_list_node.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
+from typing import Any
+
+
 class LinkedListNode:
-    def __init__(self, value=None, next=None) -> None:
+    def __init__(
+        self, value: Any = None, next: LinkedListNode | None = None
+    ) -> None:
         self.value = value
         self.next = next
 
