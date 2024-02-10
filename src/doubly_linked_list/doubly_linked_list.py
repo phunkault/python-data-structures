7c826541d0d7f62be7ea7bd8e966f9df6133999f
phunkault <phunkault@gmail.com>
2024-02-10 19:54:01 +0400

    feat: add `DoublyLinkedList.append()`

--- src/doubly_linked_list/doubly_linked_list.py
+++ src/doubly_linked_list/doubly_linked_list.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Any, Optional
 
 from src.shared.base_linked_list import BaseLinkedList
@@ -5,13 +6,25 @@ from .doubly_linked_list_node import DoublyLinkedListNode
 
 
 class DoublyLinkedList(BaseLinkedList):
-    def append(self, value: Any) -> BaseLinkedList:
+    def append(self, value: Any) -> DoublyLinkedList:
+        new_node = DoublyLinkedListNode(value)
+
+        if self._head is None:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            self._tail.next = new_node
+            new_node.prev = self._tail
+            self._tail = new_node
+
+        self._length += 1
+
+        return self
+
+    def prepend(self, value: Any) -> DoublyLinkedList:
         pass
 
-    def prepend(self, value: Any) -> BaseLinkedList:
-        pass
-
-    def insert_at(self, index: int, value: Any) -> BaseLinkedList:
+    def insert_at(self, index: int, value: Any) -> DoublyLinkedList:
         pass
 
     def delete(self, value: Any) -> Optional[DoublyLinkedListNode]:
--- tests/doubly_linked_list/test_doubly_linked_list.py
+++ tests/doubly_linked_list/test_doubly_linked_list.py
@@ -2,14 +2,63 @@ import pytest
 from src.doubly_linked_list.doubly_linked_list import DoublyLinkedList
 
 
+# Arrange
 @pytest.fixture
 def doubly_linked_list():
     return DoublyLinkedList()
 
 
+# Initial State
 def test_initial_state(doubly_linked_list):
+    # Assert
     assert doubly_linked_list.head is None
     assert doubly_linked_list.tail is None
     assert doubly_linked_list.length == 0
     assert str(doubly_linked_list) == ''
     assert doubly_linked_list.is_empty
+
+
+# Append
+def test_append_to_empty_list(doubly_linked_list):
+    # Act
+    doubly_linked_list.append(1)
+
+    # Assert
+    assert doubly_linked_list.head.data == 1
+    assert doubly_linked_list.head.prev is None
+
+    assert doubly_linked_list.tail.data == 1
+    assert doubly_linked_list.tail.next is None
+
+    assert doubly_linked_list.length == 1
+
+
+def test_append_to_non_empty_list(doubly_linked_list):
+    # Act
+    doubly_linked_list.append(1)
+    doubly_linked_list.append(2)
+
+    # Assert
+    assert doubly_linked_list.head.data == 1
+    assert doubly_linked_list.head.next.data == 2
+    assert doubly_linked_list.head.next.prev.data == 1
+
+    assert doubly_linked_list.tail.data == 2
+    assert doubly_linked_list.tail.next is None
+
+    assert doubly_linked_list.length == 2
+
+
+def test_append_in_call_chain(doubly_linked_list):
+    # Act
+    doubly_linked_list.append(1).append(2)
+
+    # Assert
+    assert doubly_linked_list.head.data == 1
+    assert doubly_linked_list.head.next.data == 2
+    assert doubly_linked_list.head.prev is None
+
+    assert doubly_linked_list.head.next.next is None
+    assert doubly_linked_list.head.next.prev.data == 1
+
+    assert doubly_linked_list.length == 2
