ea0b8c3b455bb7172efb5d849d53cd17250165b0
phunkault <phunkault@gmail.com>
2024-02-11 21:41:44 +0400

    feat: add `Dequeue.__init__()`

---
+++ src/dequeue/dequeue.py
@@ -0,0 +1,3 @@
+class Dequeue:
+    def __init__(self) -> None:
+        pass
---
+++ tests/dequeue/test_dequeue.py
@@ -0,0 +1,18 @@
+import pytest
+
+from src.dequeue.dequeue import Dequeue
+
+
+# Arrange
+@pytest.fixture
+def dequeue():
+    return Dequeue()
+
+
+# Initial state
+def test_dequeue_returns_initial_state_correctly(dequeue):
+    # Act and Assert
+    assert dequeue
+    # assert dequeue.size == 0
+    # assert str(dequeue) == ''
+    # assert dequeue.is_empty
