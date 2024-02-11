ee3ac981c0c6268faa7b91a649e8551b402601da
phunkault <phunkault@gmail.com>
2024-02-11 20:30:51 +0400

    feat: add `Queue.enqueue()`

--- src/queue/queue.py
+++ src/queue/queue.py
@@ -1,9 +1,14 @@
+from typing import Any
+
 from src.linked_list.linked_list import LinkedList
 
 
 class Queue:
     def __init__(self) -> None:
         self._list: LinkedList = LinkedList()
+
+    def __str__(self) -> str:
+        return str(self._list)
 
     @property
     def size(self) -> int:
@@ -12,3 +17,6 @@ class Queue:
     @property
     def is_empty(self):
         return self._list.is_empty
+
+    def enqueue(self, value: Any):
+        self._list.prepend(value)
--- tests/queue/test_queue.py
+++ tests/queue/test_queue.py
@@ -15,3 +15,23 @@ def test_initial_state(queue):
     assert queue
     assert queue.is_empty
     assert queue.size == 0
+
+
+# Enqueue
+def test_enqueue_single_element(queue):
+    # Act
+    queue.enqueue(10)
+
+    # Assert
+    assert queue.size == 1
+
+
+def test_enqueue_multiple_elements(queue):
+    # Act
+    queue.enqueue(10)
+    queue.enqueue(20)
+    queue.enqueue(30)
+
+    # Assert
+    assert str(queue) == '30 -> 20 -> 10'
+    assert queue.size == 3
