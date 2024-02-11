9db0463b49d4f705486c9da396d1914be0c26821
phunkault <phunkault@gmail.com>
2024-02-11 18:53:00 +0400

    feat: add `Stack.push()`

--- src/stack/stack.py
+++ src/stack/stack.py
@@ -1,15 +1,20 @@
+from __future__ import annotations
+from typing import Any
+
 from src.linked_list.linked_list import LinkedList
 
 
 class Stack:
     def __init__(self):
-        self._list = LinkedList()
-        self._size = 0
+        self._list: LinkedList = LinkedList()
 
     @property
-    def size(self) -> int:
-        return self._size
+    def length(self) -> int:
+        return self._list.length
 
     @property
     def is_empty(self) -> bool:
         return self._list.is_empty
+
+    def push(self, value: Any):
+        self._list.append(value)
--- tests/stack/test_stack.py
+++ tests/stack/test_stack.py
@@ -9,8 +9,30 @@ def stack():
     return Stack()
 
 
+# Initial state
 def test_initial_state(stack):
     # Act and Assert
     assert stack
-    assert stack.size == 0
+    assert stack.length == 0
     assert stack.is_empty
+
+
+# Push
+def test_push_single_element(stack):
+    # Act
+    stack.push(1)
+
+    # Assert
+    assert stack.length == 1
+    # assert str(stack) == '1'
+
+
+def test_push_multiple_elements(stack):
+    # Act
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    # Assert
+    assert stack.length == 3
+    # assert str(stack) == '30,20,10'
