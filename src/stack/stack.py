9a3e549aaa349621303f9146fa625706e906a584
phunkault <phunkault@gmail.com>
2024-02-11 18:26:59 +0400

    feat: add `Stack.__init__()`

---
+++ src/stack/stack.py
@@ -0,0 +1,15 @@
+from src.linked_list.linked_list import LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self._list = LinkedList()
+        self._size = 0
+
+    # @property
+    # def size(self) -> int:
+    #     return self._size
+    #
+    # @property
+    # def is_empty(self) -> bool:
+    #     return self._list.is_empty
---
+++ tests/stack/test_stack.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.stack.stack import Stack
+
+
+# Arrange
+@pytest.fixture
+def stack():
+    return Stack()
+
+
+def test_initial_state(stack):
+    # Act and Assert
+    assert stack
+    # assert stack.size == 0
+    # assert stack.is_empty
