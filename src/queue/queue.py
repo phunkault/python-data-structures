ef03014cc8d4740731685438728e254d54b3148f
phunkault <108941311+phunkault@users.noreply.github.com>
2024-02-11 21:17:30 +0400

    feat: add `Queue` (#33)
    * feat: add `Queue.__init__()`
    
    * feat: add `Queue.size`
    
    * feat: add `Queue.is_empty`
    
    * feat: add `Queue.enqueue()`
    
    * feat: add `Queue.dequeue()`
    
    * feat: add `Queue.peek()`
    
    * feat: add `Queue.clear()`

---
+++ src/queue/queue.py
@@ -0,0 +1,33 @@
+from typing import Any, Optional
+
+from src.linked_list.linked_list import LinkedList
+
+
+class Queue:
+    def __init__(self) -> None:
+        self._list: LinkedList = LinkedList()
+
+    def __str__(self) -> str:
+        return str(self._list)
+
+    @property
+    def size(self) -> int:
+        return self._list.length
+
+    @property
+    def is_empty(self) -> bool:
+        return self._list.is_empty
+
+    def enqueue(self, value: Any):
+        self._list.append(value)
+
+    def dequeue(self) -> Optional[Any]:
+        dequeued_node = self._list.delete_head()
+        return dequeued_node.data if dequeued_node else None
+
+    def peek(self) -> Optional[Any]:
+        front_element = self._list.head
+        return front_element.data if front_element else None
+
+    def clear(self) -> None:
+        self._list.clear()
