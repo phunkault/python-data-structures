41f36aeb68016608eefce769366752408f9b3db9
phunkault <108941311+phunkault@users.noreply.github.com>
2024-02-11 22:32:16 +0400

    feat: add `Dequeue` (#35)
    * feat: add `Dequeue.__init__()`
    
    * feat: add `Dequeue.size`
    
    * feat: add `Dequeue.is_empty`
    
    * feat: add `str(Deque)`
    
    * feat: add `Deque.add_front()`
    
    * feat: add `Deque.remove_front()`, `DoublyLinkedList.delete_head()`, `DoublyLinkedList.delete_tail()`
    
    * feat: add `Deque.peek_front()`
    
    * feat: add `Deque.add_rear()`
    
    * feat: add `Deque.remove_rear()`
    
    * feat: add `Deque.peek_rear()`
    
    * feat: add `Deque.peek_clear()`

---
+++ src/dequeue/dequeue.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+from src.doubly_linked_list.doubly_linked_list import DoublyLinkedList
+
+
+class Dequeue:
+    def __init__(self) -> None:
+        self._list: DoublyLinkedList = DoublyLinkedList()
+
+    def __str__(self) -> str:
+        return str(self._list)
+
+    @property
+    def size(self) -> int:
+        return self._list.length
+
+    @property
+    def is_empty(self) -> bool:
+        return self._list.is_empty
+
+    def add_front(self, value: Any):
+        self._list.prepend(value)
+        return self
+
+    def remove_front(self):
+        return self._list.delete_head()
+
+    def peek_front(self):
+        return self._list.head
+
+    def add_rear(self, value: Any):
+        self._list.append(value)
+        return self
+
+    def remove_rear(self):
+        return self._list.delete_tail()
+
+    def peek_rear(self):
+        return self._list.tail
+
+    def clear(self) -> None:
+        self._list.clear()
---
+++ tests/dequeue/test_dequeue.py
@@ -0,0 +1,104 @@
+import pytest
+
+from src.dequeue.dequeue import Dequeue
+
+
+# Arrange
+@pytest.fixture
+def dequeue():
+    return Dequeue()
+
+
+# Initial state
+def test_dequeue_returns_initial_state_correctly(dequeue):
+    # Act and Assert
+    assert dequeue
+    assert dequeue.size == 0
+    assert dequeue.is_empty
+    assert str(dequeue) == ''
+
+
+# Add front
+def test_add_front_adds_elements_to_the_front_in_correct_order(dequeue):
+    # Act
+    dequeue.add_front(2)
+
+    # Assert
+    assert str(dequeue) == '2'
+    assert dequeue.size == 1
+
+    # Act
+    dequeue.add_front(1)
+
+    # Assert
+    assert str(dequeue) == '1 -> 2'
+    assert dequeue.size == 2
+
+
+# Remove front
+def test_remove_front_removes_elements_from_the_front_in_correct_order(dequeue):
+    # Arrange
+    dequeue.add_front(1).add_front(2)
+
+    # Act and Assert
+    assert dequeue.remove_front().data == 2
+    assert dequeue.remove_front().data == 1
+    assert not dequeue.remove_front()
+    assert dequeue.size == 0
+
+
+# Peek front
+def test_peek_front_peeks_elements_from_the_front_without_removing_it(dequeue):
+    # Act and Assert
+    assert not dequeue.peek_front()
+
+    # Act and Assert
+    dequeue.add_front(2)
+    assert dequeue.peek_front().data == 2
+
+    # Act and Assert
+    dequeue.add_front(1)
+    assert dequeue.peek_front().data == 1
+
+
+# Add rear
+def test_add_rear_adds_elements_to_the_rear_in_correct_order(dequeue):
+    # Act
+    dequeue.add_rear(1)
+
+    # Assert
+    assert str(dequeue) == '1'
+    assert dequeue.size == 1
+
+    # Act
+    dequeue.add_rear(2)
+
+    # Assert
+    assert str(dequeue) == '1 -> 2'
+    assert dequeue.size == 2
+
+
+# Remove rear
+def test_remove_rear_removes_elements_from_the_rear_in_correct_order(dequeue):
+    # Arrange
+    dequeue.add_rear(1).add_rear(2)
+
+    # Act and Assert
+    assert dequeue.remove_rear().data == 2
+    assert dequeue.remove_rear().data == 1
+    assert not dequeue.remove_rear()
+    assert dequeue.size == 0
+
+
+# Peek rear
+def test_peek_rear_peeks_elements_from_the_rear_without_removing_it(dequeue):
+    # Act and Assert
+    assert not dequeue.peek_rear()
+
+    # Act and Assert
+    dequeue.add_rear(1)
+    assert dequeue.peek_rear().data == 1
+
+    # Act and Assert
+    dequeue.add_rear(2)
+    assert dequeue.peek_rear().data == 2
