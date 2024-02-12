f4b67c0f30d6153d161dbcaff8446ad893dec9d6
phunkault <phunkault@gmail.com>
2024-02-12 18:34:21 +0400

    feat: add `BinaryTree.insert()`

--- src/tree/binary_tree.py
+++ src/tree/binary_tree.py
@@ -1,10 +1,26 @@
+from typing import Optional, Any
+
 from .binary_tree_node import BinaryTreeNode
 
 
 class BinaryTree:
-    def __init__(self, root: BinaryTreeNode = None) -> None:
+    def __init__(self, root: Optional[BinaryTreeNode] = None) -> None:
         self._root = root
 
     @property
-    def root(self):
+    def root(self) -> BinaryTreeNode:
         return self._root
+
+    def insert(self, value: Any) -> None:
+        self._root = self._insert(self.root, value=value)
+
+    def _insert(self, node: BinaryTreeNode, value: Any) -> BinaryTreeNode:
+        if not node:
+            return BinaryTreeNode(value)
+        if not node.left:
+            node.left = self._insert(node.left, value)
+        elif not node.right:
+            node.right = self._insert(node.right, value)
+        else:
+            node.left = self._insert(node.left, value)
+        return node
--- tests/tree/test_binary_tree.py
+++ tests/tree/test_binary_tree.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.tree.binary_tree import BinaryTree
+from src.tree.binary_tree_node import BinaryTreeNode
 
 
 # Arrange
@@ -14,3 +15,28 @@ def test_initial_state(binary_tree):
     # Assert
     assert binary_tree
     assert not binary_tree.root
+
+
+# Insert
+def test_insert_in_empty_tree(binary_tree):
+    # Act
+    binary_tree.insert(1)
+
+    # Assert
+    assert binary_tree.root.value == 1
+
+
+def test_insert_in_non_empty_tree():
+    # Arrange
+    tree = BinaryTree(BinaryTreeNode(1))
+
+    # Act
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+
+    # Assert
+    assert tree.root.value == 1
+    assert tree.root.left.value == 2
+    assert tree.root.right.value == 3
+    assert tree.root.left.left.value == 4
