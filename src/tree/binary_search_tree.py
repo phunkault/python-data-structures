1284abf4253046357a94ce4a4cdbf45ea1cff63a
phunkault <phunkault@gmail.com>
2024-02-13 12:28:40 +0400

    feat: add `BinarySearchTree.insert()`

--- src/tree/binary_search_tree.py
+++ src/tree/binary_search_tree.py
@@ -1,8 +1,28 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Optional, Any
 
 from .binary_tree_node import BinaryTreeNode
 
 
 class BinarySearchTree:
     def __init__(self, root: Optional[BinaryTreeNode] = None) -> None:
-        self.root = root
+        self._root = root
+
+    @property
+    def root(self) -> Optional[BinaryTreeNode]:
+        return self._root
+
+    def insert(self, key: Any) -> BinarySearchTree:
+        self._root = self._insert(self.root, key)
+        return self
+
+    def _insert(self, root: BinaryTreeNode, key: Any) -> BinaryTreeNode:
+        if not root:
+            return BinaryTreeNode(key)
+        if key < root.value:
+            root.left = self._insert(root.left, key)
+        elif key > root.value:
+            root.right = self._insert(root.right, key)
+
+        return root
--- tests/tree/test_binary_search_tree.py
+++ tests/tree/test_binary_search_tree.py
@@ -14,3 +14,26 @@ def test_initial_state_is_correct(bst):
     # Assert
     assert bst
     assert not bst.root
+
+
+# Insert
+def test_insert_in_empty_tree(bst):
+    # Act
+    bst.insert(1)
+
+    # Assert
+    assert bst.root.value == 1
+    assert not bst.root.left
+    assert not bst.root.right
+
+
+def test_insert_call_chain(bst):
+    # Act
+    bst.insert(8).insert(3).insert(10).insert(1).insert(6)
+
+    # Assert
+    assert bst.root.value == 8
+    assert bst.root.left.value == 3
+    assert bst.root.right.value == 10
+    assert bst.root.left.left.value == 1
+    assert bst.root.left.right.value == 6
