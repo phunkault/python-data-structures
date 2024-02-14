0a1790a082d82dd94474b7f83c284c64f672a739
phunkault <phunkault@gmail.com>
2024-02-14 20:42:39 +0400

    feature: add `AVLTree.get_min_node()`

--- src/tree/avl_tree.py
+++ src/tree/avl_tree.py
@@ -16,3 +16,9 @@ class AVLTree(BinarySearchTree):
         bf = self.get_height(node.left) - self.get_height(node.right)
 
         return 0 if not node else bf
+
+    def get_min_node(self, node: Optional[AVLTreeNode]) -> AVLTreeNode:
+        if not node or not node.left:
+            return node
+        else:
+            return self.get_min_node(node.left)
