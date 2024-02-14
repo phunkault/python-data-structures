2931c41d54b0975e550bd4a06374a2d93cb3a66d
phunkault <phunkault@gmail.com>
2024-02-14 20:28:59 +0400

    feature: add `AVLTree.get_height()`

--- src/tree/avl_tree.py
+++ src/tree/avl_tree.py
@@ -7,3 +7,7 @@ from .avl_tree_node import AVLTreeNode
 class AVLTree(BinarySearchTree):
     def __init__(self, root: Optional[AVLTreeNode] = None) -> None:
         super().__init__(root)
+
+    @staticmethod
+    def get_height(node: Optional[AVLTreeNode]) -> int:
+        return 0 if not node else node.height
--- tests/tree/test_avl_tree.py
+++ tests/tree/test_avl_tree.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.tree.avl_tree_node import AVLTreeNode
 from src.tree.avl_tree import AVLTree
 
 
@@ -14,3 +15,17 @@ def test_avl_tree_initial_state(avl_tree):
     # Assert
     assert avl_tree
     assert not avl_tree.root
+
+
+# Get height
+def test_get_height_of_empty_tree(avl_tree):
+    # Assert
+    assert not avl_tree.get_height(avl_tree.root)
+
+
+def test_get_height_of_tree_with_one_node():
+    # Arrange
+    tree = AVLTree(AVLTreeNode(1))
+
+    # Assert
+    assert tree.get_height(tree.root) == 1
