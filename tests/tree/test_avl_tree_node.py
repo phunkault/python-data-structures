93ec9cbde3ac7bee9552a015d8cca2b9e4cc5314
phunkault <phunkault@gmail.com>
2024-02-14 18:51:43 +0400

    feature: add `AVLTreeNode.__init__()`

---
+++ tests/tree/test_avl_tree_node.py
@@ -0,0 +1,14 @@
+from src.tree.avl_tree_node import AVLTreeNode
+
+
+# Initial state
+def test_avl_tree_node_initial_state():
+    # Arrange
+    node = AVLTreeNode()
+
+    # Assert
+    assert node
+    assert not node.value
+    assert not node.left
+    assert not node.right
+    assert node.height == 1
