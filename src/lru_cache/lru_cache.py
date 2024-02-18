b8de81e6f78ed8e1348d68952b0e3974b0c5b371
phunkault <phunkault@gmail.com>
2024-02-18 19:14:38 +0400

    feature: add `LRUCache.put()`

--- src/lru_cache/lru_cache.py
+++ src/lru_cache/lru_cache.py
@@ -1,9 +1,30 @@
-from src.doubly_linked_list.doubly_linked_list import DoublyLinkedList
+from __future__ import annotations
+
+from src.doubly_linked_list.doubly_linked_list import (
+    DoublyLinkedList,
+    DoublyLinkedListNode,
+)
 
 
 class LRUCache:
     def __init__(self, capacity: int) -> None:
-        self.size: int = 0
         self.capacity = capacity
         self.list: DoublyLinkedList = DoublyLinkedList()
         self.node_map: dict = {}
+
+    def put(self, data) -> LRUCache:
+        if data in self.node_map:
+            self.list.delete(self.node_map[data])
+
+        node = DoublyLinkedListNode(data)
+
+        self.list.append(node)
+
+        self.node_map[data] = node
+
+        if self.list.length > self.capacity:
+            self.list.delete_head()
+
+            del self.node_map[data]
+
+        return self
--- tests/lru_cache/test_lru_cache.py
+++ tests/lru_cache/test_lru_cache.py
@@ -13,7 +13,47 @@ def lru_cache():
 def test_lru_cache_initial_state(lru_cache):
     # Assert
     assert lru_cache
-    assert lru_cache.size == 0
+
     assert lru_cache.capacity == 3
+
     assert lru_cache.list.is_empty
+
     assert not lru_cache.node_map
+
+
+# Put
+def test_lru_cache_put(lru_cache):
+    # Act
+    lru_cache.put(1)
+    lru_cache.put(2)
+
+    # Assert
+    assert lru_cache.list.length == 2
+
+    assert len(lru_cache.node_map) == 2
+
+
+def test_put_update_value(lru_cache):
+    # Arrange
+    lru_cache.put(1)
+
+    # Act
+    lru_cache.put(1)
+
+    # Assert
+    assert lru_cache.list.length == 1
+
+
+def test_lru_cache_put_overflow(lru_cache):
+    # Arrange
+    lru_cache.put(1).put(2).put(3)
+
+    # Actr
+    lru_cache.put(4)
+
+    # Assert
+    assert lru_cache.list.length == 3
+
+    assert len(lru_cache.node_map) == 3
+
+    assert str(lru_cache.list) == "2 -> 3 -> 4"
