906321c82f8862c3979c26d1794dfe6a74d932a6
phunkault <phunkault@gmail.com>
2024-02-22 16:07:11 +0400

    feature: add `MFUCache.put()`

--- src/mfu_cache/mfu_cache.py
+++ src/mfu_cache/mfu_cache.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Any
 
 from src.doubly_linked_list.doubly_linked_list_node import DoublyLinkedListNode
@@ -7,14 +9,25 @@ from src.doubly_linked_list.doubly_linked_list import DoublyLinkedList
 DEFAULT_CAPACITY = 3
 
 
+class NodeData:
+    def __init__(self, key: Any, value: Any) -> None:
+        self.key = key
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.key}, {self.value}"
+
+
 class MFUCache:
     def __init__(self) -> None:
         self._capacity: int = DEFAULT_CAPACITY
         self._size: int = 0
         self._max_freq: int = 1
-        self._node_map: dict[Any, DoublyLinkedListNode]
-        self._freq_map: dict[Any, int]
-        self._buckets: dict[int, DoublyLinkedList]
+        self._node_map: dict[Any, DoublyLinkedListNode] = {}
+        self._freq_map: dict[Any, int] = {}
+        self._buckets: dict[int, DoublyLinkedList] = {
+            self._max_freq: DoublyLinkedList()
+        }
 
     @property
     def size(self) -> int:
@@ -23,3 +36,38 @@ class MFUCache:
     @property
     def capacity(self) -> int:
         return self._capacity
+
+    def _delete_mfu_node(self):
+        mfu_node_bucket = self._buckets[self._max_freq]
+        mfu_node = mfu_node_bucket.delete_tail()
+
+        del self._node_map[mfu_node.data.key]
+        del self._freq_map[mfu_node.data.key]
+
+        self._size -= 1
+
+    def put(self, key: Any, value: Any) -> MFUCache:
+        if key in self._node_map:
+            # renew value
+            node = self._node_map[key]
+            node.data.value = value
+            # update frequency
+            return self
+
+        # MFUCache is loaded
+        if self._size == self.capacity:
+            # delete MFU
+            self._delete_mfu_node()
+
+        new_node_data = NodeData(key, value)
+
+        self._freq_map[key] = 1
+
+        bucket = self._buckets[1]
+        bucket.append(new_node_data)
+
+        self._node_map[key] = bucket.tail
+
+        self._size += 1
+
+        return self
--- tests/mfu_cache/test_mfu_cache.py
+++ tests/mfu_cache/test_mfu_cache.py
@@ -15,3 +15,56 @@ def test_mfu_cache_initial_state(mfu_cache):
     assert mfu_cache
     assert mfu_cache.size == 0
     assert mfu_cache.capacity == 3
+
+
+# Put
+def test_put_in_empty_mfu_cache(mfu_cache):
+    # Act
+    mfu_cache.put("one", 1)
+
+    # Assert
+    assert mfu_cache.size == 1
+
+
+def test_put_in_non_loaded_cache(mfu_cache):
+    # Arrange
+    mfu_cache.put("one", 1)
+    mfu_cache.put("two", 2)
+
+    # Act
+    mfu_cache.put("three", 3)
+
+    # Assert
+    assert mfu_cache.size == 3
+
+
+def test_put_in_loaded_mfu_cache(mfu_cache):
+    # Arrange
+    mfu_cache.put("one", 1)
+    mfu_cache.put("two", 2)
+    mfu_cache.put("three", 3)
+
+    # Act
+    mfu_cache.put("four", 4)
+
+    # Assert
+    assert mfu_cache.size == 3
+
+
+def test_put_same_key_in_mfu_cache_renews_value(mfu_cache):
+    # Arrange
+    mfu_cache.put("one", 1)
+
+    # Act
+    mfu_cache.put("one", 10)
+
+    # Assert
+    assert mfu_cache.size == 1
+
+
+def test_put_in_mfu_cache_call_chain(mfu_cache):
+    # Act
+    mfu_cache.put("one", 1).put("two", 2).put("three", 3)
+
+    # Assert
+    assert mfu_cache.size == 3
