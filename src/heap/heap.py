6fbe6f2ab57e8bb1793febeff5f416cb64d66d73
phunkault <phunkault@gmail.com>
2024-02-24 13:47:57 +0400

    feature: add tests

--- src/heap/heap.py
+++ src/heap/heap.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
-from typing import List, Any, Optional
+from typing import List, Any
 
 
-class Heap:
-    """Base Heap class"""
-
-    pass
-
+class MinHeap:
     def __init__(self) -> None:
         self._container: List[Any] = []
         self._size: int = 0
@@ -26,7 +22,7 @@ class Heap:
 
     @property
     def is_empty(self) -> bool:
-        return True if self._size == 0 else False
+        return True if self.size == 0 else False
 
     @staticmethod
     def _get_left_child_index(parent_index: int) -> int:
@@ -40,11 +36,16 @@ class Heap:
     def _get_parent_index(child_index: int) -> int:
         return (child_index - 1) // 2
 
+    @staticmethod
+    def _compare(first_value: Any, second_value: Any) -> bool:
+        # for MaxHeap this should be opposite
+        return first_value <= second_value
+
     def _has_left_child(self, parent_index: int) -> bool:
-        return self._get_left_child_index(parent_index) < len(self.container)
+        return self._get_left_child_index(parent_index) < self.size
 
     def _has_right_child(self, parent_index: int) -> bool:
-        return self._get_right_child_index(parent_index) < len(self.container)
+        return self._get_right_child_index(parent_index) < self.size
 
     def _has_parent(self, index: int) -> bool:
         return self._get_parent_index(index) >= 0
@@ -62,28 +63,26 @@ class Heap:
         return self.container[self._get_parent_index(child_index)]
 
     def _swap(self, first_idx: int, second_idx: int) -> None:
-        if first_idx >= self.size:
-            raise IndexError(f"{first_idx} is out of heap.")
-        elif second_idx >= self.size:
-            raise IndexError(f"{second_idx} is out of heap.")
+        if first_idx >= self.size or second_idx >= self.size:
+            return
         else:
             tmp = self.container[first_idx]
             self.container[first_idx] = self.container[second_idx]
             self.container[second_idx] = tmp
 
     def peek(self) -> Any:
-        return self._container[0] if not self._is_empty else None
+        return self._container[0] if not self.is_empty else None
 
     def find(self, value: Any) -> List[int]:
         found_indices = []
 
-        for el in self.container:
-            if el == value:
-                found_indices.append(self.container.index(el))
+        for idx, item in enumerate(self.container):
+            if item == value:
+                found_indices.append(idx)
 
         return found_indices
 
-    def push(self, value: Any) -> Heap:
+    def push(self, value: Any) -> MinHeap:
         self._container.append(value)
         self.heapify_up()
 
@@ -103,27 +102,27 @@ class Heap:
 
         return replaced
 
-    def heapify_up(self, custom_start_index: Optional[int] = None) -> None:
-        cur_idx = custom_start_index or len(self.container) - 1
+    def heapify_up(self, custom_start_index=None) -> None:
+        cur_idx = custom_start_index or self.size - 1
 
-        while self._has_parent(cur_idx) and not self._correct_order(
+        while self._has_parent(cur_idx) and not MinHeap._compare(
             self._get_parent(cur_idx), self.container[cur_idx]
         ):
             self._swap(cur_idx, self._get_parent_index(cur_idx))
             cur_idx = self._get_parent_index(cur_idx)
 
-    def heapify_down(self) -> None:
-        cur_idx = 0
+    def heapify_down(self, custom_start_index=0) -> None:
+        cur_idx = custom_start_index
 
         while self._has_left_child(cur_idx):
-            if self._has_right_child(cur_idx) and self._correct_order(
+            if self._has_right_child(cur_idx) and MinHeap._compare(
                 self._get_right_child(cur_idx), self._get_left_child(cur_idx)
             ):
                 next_index = self._get_right_child_index(cur_idx)
             else:
                 next_index = self._get_left_child_index(cur_idx)
 
-            if self._correct_order(
+            if MinHeap._compare(
                 self.container[cur_idx], self.container[next_index]
             ):
                 break
@@ -132,11 +131,25 @@ class Heap:
 
             cur_idx = next_index
 
-    def remove(self):
-        pass
+    def remove(self, value: Any) -> MinHeap:
+        indices_to_remove = self.find(value)
+
+        for idx in indices_to_remove[::-1]:
+            if idx == self.size - 1:
+                self.container.pop()
+            else:
+                self.container[idx] = self.container.pop()
+                parent = self._get_parent(idx)
+
+                if self._has_left_child(idx) and (
+                    not parent or MinHeap._compare(parent, self.container[idx])
+                ):
+                    self.heapify_down(idx)
+                else:
+                    self.heapify_up(idx)
+
+        return self
 
     def merge(self):
-        pass
-
-    def _correct_order(self, first_element, second_element):
+        # merge two heaps into a new one
         pass
--- tests/heap/test_heap.py
+++ tests/heap/test_heap.py
@@ -1,12 +1,12 @@
 import pytest
 
-from src.heap.heap import Heap
+from src.heap.heap import MinHeap
 
 
 # Arrange
 @pytest.fixture()
 def heap():
-    return Heap()
+    return MinHeap()
 
 
 # Initial state
@@ -26,5 +26,158 @@ def test_heap_to_str(heap):
 
 
 # Find
-def test_heap_find(heap):
-    assert heap.find(1) == []
+def test_min_heap_find_empty_heap(heap):
+    # Act
+    result = heap.find(1)
+
+    # Assert
+    assert result == []
+
+
+def test_min_heap_find_value_not_present(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7).push(3)
+
+    # Act
+    result = heap.find(6)
+
+    # Assert
+    assert result == []
+
+
+def test_min_heap_find_value_present(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7).push(3)
+
+    # Act
+    result = heap.find(3)
+
+    # Assert
+    assert result == [1, 2]
+
+
+def test_min_heap_find_single_element_heap(heap):
+    # Arrange
+    heap.push(42)
+
+    # Act
+    result = heap.find(42)
+
+    assert result == [0]
+
+
+# Push
+def test_push_one_element(heap):
+    # Act
+    heap.push(4)
+
+    # Assert
+    assert heap.size == 1
+    assert not heap.is_empty
+    assert heap.peek() == 4
+
+
+def test_push_multiple_elements(heap):
+    # Arrange
+    heap.push(2)
+
+    # Act
+    heap.push(7).push(1).push(5)
+
+    # Assert
+    assert heap.size == 4
+    assert heap.peek() == 1
+
+
+# Replace
+def test_replace_in_empty_heap(heap):
+    # Act
+    assert not heap.replace()
+
+
+def test_replace_in_heap_with_one_element(heap):
+    # Arrange
+    heap.push(1)
+    # Act
+    replaced_value = heap.replace()
+
+    # Assert
+    assert replaced_value == 1
+    assert heap.size == 0
+    assert not heap.peek()
+
+
+def test_replace_in_heap_with_several_elements(heap):
+    # Arrange
+    heap.push(1).push(2).push(3).push(4).push(5).push(6).push(7)
+
+    # Act
+    replaced_value = heap.replace()
+
+    # Assert
+    assert replaced_value == 1
+    assert heap.size == 6
+    assert heap.peek() == 2
+
+
+# Remove
+def test_min_heap_remove_empty_heap(heap):
+    # Act
+    heap.remove(5)
+
+    # Assert
+    assert heap.container == []
+
+
+def test_min_heap_remove_nonexistent_value(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7)
+
+    # Act
+    heap.remove(4)
+
+    # Assert
+    assert heap.container == [2, 3, 8, 5, 7]
+
+
+def test_min_heap_remove_single_element_heap(heap):
+    # Assert
+    heap.push(5)
+
+    # Act
+    heap.remove(5)
+
+    # Assert
+    assert heap.container == []
+
+
+def test_min_heap_remove_last_element(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7)
+
+    # Act
+    heap.remove(7)
+
+    # Assert
+    assert heap.container == [2, 3, 8, 5]
+
+
+def test_min_heap_remove_middle_element(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7)
+
+    # Act
+    heap.remove(3)
+
+    assert heap.container == [2, 5, 8, 7]
+
+
+def test_min_heap_remove_multiple_occurrences(heap):
+    # Arrange
+    heap.push(5).push(3).push(8).push(2).push(7).push(3)
+
+    # Act
+    heap.remove(3)
+
+    # Assert
+    assert heap.container == [2, 5, 8, 7]
