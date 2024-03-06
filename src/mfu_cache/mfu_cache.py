1e65a8d3c85f9858420d2533d4b7b7b6848b751c
phunkault <phunkault@gmail.com>
2024-03-06 20:15:33 +0400

    refactor: rewrite cache with dicts

--- src/mfu_cache/mfu_cache.py
+++ src/mfu_cache/mfu_cache.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, List
 
 from src.doubly_linked_list.doubly_linked_list_node import DoublyLinkedListNode
 from src.doubly_linked_list.doubly_linked_list import DoublyLinkedList
@@ -19,22 +19,22 @@ class NodeData:
 
 
 class MFUCache:
-    def __init__(self) -> None:
-        self._capacity: int = DEFAULT_CAPACITY
+    def __init__(self, capacity: int = DEFAULT_CAPACITY) -> None:
+        self._capacity = capacity
         self._size: int = 0
         self._max_freq: int = 1
-        self._node_map: dict[Any, DoublyLinkedListNode] = {}
-        self._freq_map: dict[Any, int] = {}
-        self._buckets: dict[int, DoublyLinkedList] = {
-            self._max_freq: DoublyLinkedList()
-        }
+        self._key_node_map: dict[Any, DoublyLinkedListNode] = {}
+        self._key_freq_map: dict[Any, int] = {}
+        self._freq_list_map: dict[int, DoublyLinkedList] = {}
+        self._buckets: DoublyLinkedList = DoublyLinkedList()
+        self._add_first_bucket()
 
-    def __str__(self) -> str:
-        string = ""
-        for bucket in self._buckets:
-            freq_str = f"Freq {bucket}: {str(self._buckets[bucket])}\n"
-            string += freq_str
-        return string
+    def _add_first_bucket(self):
+        # bucket is a key-value pair – {frequency: DLL}
+        first_freq_list = DoublyLinkedList()
+        first_bucket = {self.max_freq: first_freq_list}
+        self._buckets.append(first_bucket)
+        self._freq_list_map[self.max_freq] = first_freq_list
 
     @property
     def size(self) -> int:
@@ -44,63 +44,87 @@ class MFUCache:
     def capacity(self) -> int:
         return self._capacity
 
-    def _delete_mfu_node(self) -> None:
-        mfu_node_bucket = self._buckets[self._max_freq]
-        mfu_node = mfu_node_bucket.delete_tail()
+    @property
+    def max_freq(self) -> int:
+        return self._max_freq
 
-        del self._node_map[mfu_node.data.key]
-        del self._freq_map[mfu_node.data.key]
+    def _delete_mfu_node(self) -> None:
+        mfu_node_freq_list = self._buckets.tail.data[self._max_freq]
+
+        mfu_node = mfu_node_freq_list.delete_head()
+
+        del self._key_node_map[mfu_node.data.key]
+        del self._key_freq_map[mfu_node.data.key]
+
+        # update max_freq if mfu_bucket becomes empty
+        if mfu_node_freq_list.is_empty:
+            # delete the reference of mfu_node_freq_list first
+            del self._freq_list_map[self._max_freq]
+            # get previous max frequency, means the previous bucket frequency
+            prev_max_freq = next(iter(self._buckets.tail.prev.data))
+            # override max frequency
+            self._max_freq = prev_max_freq
+            # delete the current max frequency bucket
+            self._buckets.delete_tail()
 
         self._size -= 1
 
     def _update_frequency(self, key: Any) -> None:
-        node = self._node_map[key]
+        node = self._key_node_map[key]
         node_data = NodeData(node.data.key, node.data.value)
 
-        old_freq = self._freq_map[key]
+        old_freq = self._key_freq_map[key]
 
-        old_bucket = self._buckets[old_freq]
-        old_bucket.delete_by_ref(node)
+        old_freq_list = self._freq_list_map[old_freq]
+        old_freq_list.delete_by_ref(node)
 
         new_freq = old_freq + 1
 
         try:
-            new_bucket = self._buckets[new_freq]
+            new_freq_list = self._freq_list_map[new_freq]
         except KeyError:
-            new_bucket = DoublyLinkedList()
-            self._buckets[new_freq] = new_bucket
+            # if there is no new_freq_list we must create it
+            new_freq_list = DoublyLinkedList()
+            self._freq_list_map[new_freq] = new_freq_list
 
-        new_bucket.append(node_data)
+            # and add a new bucket to buckets
+            new_bucket = {new_freq: new_freq_list}
+            self._buckets.append(new_bucket)
 
-        self._node_map[key] = new_bucket.tail
-        self._freq_map[key] = new_freq
+        # add node to a new freq_list
+        new_freq_list.append(node_data)
 
+        # override node's references
+        self._key_node_map[key] = new_freq_list.tail
+        self._key_freq_map[key] = new_freq
+
+        # if new_freq > current max frequency, means we must override it
         if new_freq > self._max_freq:
             self._max_freq = new_freq
 
     def put(self, key: Any, value: Any) -> MFUCache:
-        if key in self._node_map:
-            # renew value
-            node = self._node_map[key]
+        if key in self._key_node_map:
+            # override value
+            node = self._key_node_map[key]
             node.data.value = value
             # update frequency
             self._update_frequency(key)
 
             return self
 
-        # MFUCache is loaded
+        # MFUCache is loaded, means we should delete the MFU
         if self._size == self.capacity:
-            # delete MFU
             self._delete_mfu_node()
 
+        # new nodes always go to the first bucket (with frequency 1)
         new_node_data = NodeData(key, value)
 
-        self._freq_map[key] = 1
+        freq_list = self._freq_list_map[1]
+        freq_list.append(new_node_data)
 
-        bucket = self._buckets[1]
-        bucket.append(new_node_data)
-
-        self._node_map[key] = bucket.tail
+        # add node's references
+        self._key_freq_map[key] = 1
+        self._key_node_map[key] = freq_list.tail
 
         self._size += 1
 
@@ -108,12 +132,20 @@ class MFUCache:
 
     def get(self, key: Any) -> Any:
         # key doesn't exist
-        if key not in self._node_map:
+        if key not in self._key_node_map:
             return None
 
-        node = self._node_map[key]
+        node = self._key_node_map[key]
         value = node.data.value
         # update frequency
         self._update_frequency(key)
 
         return value
+
+    def to_array(self) -> List[Any]:
+        array = []
+        for bucket in self._buckets:
+            freq_list = list(bucket.data.values())[0]
+            for node in freq_list:
+                array.append(node.data.key)
+        return array
--- tests/mfu_cache/test_mfu_cache.py
+++ tests/mfu_cache/test_mfu_cache.py
@@ -5,119 +5,161 @@ from src.mfu_cache.mfu_cache import MFUCache
 
 # Arrange
 @pytest.fixture()
-def mfu_cache():
-    return MFUCache()
+def mfu_cache_cap_2():
+    return MFUCache(2)
+
+
+@pytest.fixture()
+def mfu_cache_cap_3():
+    return MFUCache(3)
 
 
 # Initial state
-def test_mfu_cache_initial_state(mfu_cache):
+def test_mfu_cache_initial_state(mfu_cache_cap_2):
     # Assert
-    assert mfu_cache
-    assert mfu_cache.size == 0
-    assert mfu_cache.capacity == 3
+    assert mfu_cache_cap_2
+    assert mfu_cache_cap_2.size == 0
+    assert mfu_cache_cap_2.capacity == 2
+    assert mfu_cache_cap_2.max_freq == 1
 
 
 # Put
-def test_put_in_empty_mfu_cache(mfu_cache):
+def test_put_in_empty_mfu_cache(mfu_cache_cap_2):
     # Act
-    mfu_cache.put("one", 1)
+    mfu_cache_cap_2.put("one", 1)
 
     # Assert
-    assert mfu_cache.size == 1
+    assert mfu_cache_cap_2.size == 1
+    assert mfu_cache_cap_2.max_freq == 1
 
 
-def test_put_in_non_loaded_cache(mfu_cache):
+def test_put_in_non_loaded_cache(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1)
-    mfu_cache.put("two", 2)
+    mfu_cache_cap_2.put("one", 1)
 
     # Act
-    mfu_cache.put("three", 3)
+    mfu_cache_cap_2.put("two", 2)
 
     # Assert
-    assert mfu_cache.size == 3
+    assert mfu_cache_cap_2.size == 2
+    assert mfu_cache_cap_2.max_freq == 1
 
 
-def test_put_in_loaded_mfu_cache(mfu_cache):
+def test_put_in_loaded_mfu_cache(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1)
-    mfu_cache.put("two", 2)
-    mfu_cache.put("three", 3)
+    mfu_cache_cap_2.put("one", 1)
+    mfu_cache_cap_2.put("two", 2)
 
     # Act
-    mfu_cache.put("four", 4)
+    mfu_cache_cap_2.put("three", 3)
 
     # Assert
-    assert mfu_cache.size == 3
+    assert mfu_cache_cap_2.size == 2
+    assert mfu_cache_cap_2.max_freq == 1
 
 
-def test_put_same_key_in_mfu_cache_renews_value(mfu_cache):
+def test_put_same_key_in_mfu_cache_overrides_value(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1)
+    mfu_cache_cap_2.put("one", 1)
 
     # Act
-    mfu_cache.put("one", 10)
+    mfu_cache_cap_2.put("one", 10)
 
     # Assert
-    assert mfu_cache.size == 1
+    assert mfu_cache_cap_2.size == 1
+    assert mfu_cache_cap_2.max_freq == 2
 
 
-def test_put_in_mfu_cache_call_chain(mfu_cache):
+def test_put_in_mfu_cache_call_chain(mfu_cache_cap_2):
     # Act
-    mfu_cache.put("one", 1).put("two", 2).put("three", 3)
-
+    mfu_cache_cap_2.put("one", 1).put("two", 2)
     # Assert
-    assert mfu_cache.size == 3
+    assert mfu_cache_cap_2.size == 2
+    assert mfu_cache_cap_2.max_freq == 1
 
 
 # Get
-def test_get_existing_node_value(mfu_cache):
+def test_get_existing_node_value(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1)
+    mfu_cache_cap_2.put("one", 1)
 
     # Act and assert
-    assert mfu_cache.get("one") == 1
-    assert mfu_cache.size == 1
+    assert mfu_cache_cap_2.get("one") == 1
+    assert mfu_cache_cap_2.size == 1
+    assert mfu_cache_cap_2.max_freq == 2
 
 
-def test_get_non_existing_node_value(mfu_cache):
+def test_get_non_existing_node_value(mfu_cache_cap_2):
     # Assert
-    assert not mfu_cache.get("one")
-    assert mfu_cache.size == 0
+    assert not mfu_cache_cap_2.get("one")
+    assert mfu_cache_cap_2.size == 0
 
 
-def test_get_all_existing_node_values(mfu_cache):
+def test_get_all_existing_node_values(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1).put("two", 2).put("three", 3)
+    mfu_cache_cap_2.put("one", 1).put("two", 2)
 
     # Act and assert
-    assert mfu_cache.get("one") == 1
-    assert mfu_cache.get("two") == 2
-    assert mfu_cache.get("three") == 3
+    assert mfu_cache_cap_2.get("one") == 1
+    assert mfu_cache_cap_2.get("two") == 2
+
+    assert mfu_cache_cap_2.max_freq == 2
 
 
-# Update frequency
-def test_update_frequency(mfu_cache):
+# Max's tests with two nodes
+def test_max_two_nodes_first(mfu_cache_cap_2):
     # Arrange
-    mfu_cache.put("one", 1).put("two", 2).put("three", 3)
-
-    cache_str = "Freq 1: one, 1 -> two, 2 -> three, 3\n"
-    assert str(mfu_cache) == cache_str
-
-    mfu_cache.get("one")
-    mfu_cache.get("two")
-    mfu_cache.get("three")
-
-    cache_str = "Freq 1: \nFreq 2: one, 1 -> two, 2 -> three, 3\n"
-    assert str(mfu_cache) == cache_str
+    mfu_cache_cap_2.put("a", 1)
+    mfu_cache_cap_2.put("b", 2)
 
     # Act
-    mfu_cache.get("three")
+    mfu_cache_cap_2.put("c", 3)
 
-    cache_str = "Freq 1: \nFreq 2: one, 1 -> two, 2\nFreq 3: three, 3\n"
-    assert str(mfu_cache) == cache_str
+    # Assert
+    assert mfu_cache_cap_2.max_freq == 1
+    assert mfu_cache_cap_2.to_array() == ["b", "c"]
 
-    mfu_cache.put("four", 4)
 
-    cache_str = "Freq 1: four, 4\nFreq 2: one, 1 -> two, 2\nFreq 3: \n"
-    assert str(mfu_cache) == cache_str
+def test_max_two_nodes_second(mfu_cache_cap_2):
+    # Arrange
+    mfu_cache_cap_2.put("a", 1)
+    mfu_cache_cap_2.put("b", 2)
+
+    # Act
+    mfu_cache_cap_2.put("b", 22)
+    mfu_cache_cap_2.put("c", 3)
+
+    # Assert
+    assert mfu_cache_cap_2.max_freq == 1
+    assert mfu_cache_cap_2.to_array() == ["a", "c"]
+
+
+def test_max_two_nodes_third(mfu_cache_cap_2):
+    # Arrange
+    mfu_cache_cap_2.put("a", 1)
+    mfu_cache_cap_2.put("b", 2)
+
+    mfu_cache_cap_2.put("a", 11)
+    mfu_cache_cap_2.put("b", 22)
+
+    # Act
+    mfu_cache_cap_2.put("c", 3)
+
+    # Assert
+    assert mfu_cache_cap_2.max_freq == 2
+    assert mfu_cache_cap_2.to_array() == ["c", "b"]
+
+
+# Max's tests with three nodes
+def test_max_three_nodes_first(mfu_cache_cap_3):
+    # Arrange
+    mfu_cache_cap_3.put("a", 1)
+    mfu_cache_cap_3.put("b", 2)
+    mfu_cache_cap_3.put("c", 3)
+
+    # Act
+    mfu_cache_cap_3.put("c", 33)
+
+    # Assert
+    assert mfu_cache_cap_3.max_freq == 2
+    assert mfu_cache_cap_3.to_array() == ["a", "b", "c"]
