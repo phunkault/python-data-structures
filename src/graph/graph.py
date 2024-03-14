f7020e5df5b659c27e959e9473476f809a1ebaab
phunkault <phunkault@gmail.com>
2024-03-14 17:54:12 +0400

    feature: add Vertex.add_neighbor() in graph.py

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -5,3 +5,7 @@ class Vertex:
     def __init__(self, v_id: Any = None) -> None:
         self.id = v_id
         self.neighbors: list[Vertex] = []
+
+    def add_neighbor(self, neighbor: "Vertex") -> None:
+        if neighbor not in self.neighbors:
+            self.neighbors.append(neighbor)
--- tests/graph/test_graph.py
+++ tests/graph/test_graph.py
@@ -4,13 +4,28 @@ from src.graph.graph import Vertex
 
 # Arrange
 @pytest.fixture()
-def vertex():
+def vertexA():
     return Vertex("A")
 
 
 # Vertex
-def test_vertex_initial_state(vertex):
+def test_vertex_initial_state(vertexA):
     # Assert
-    assert vertex
-    assert vertex.id == "A"
-    assert not vertex.neighbors
+    assert vertexA
+    assert vertexA.id == "A"
+    assert not vertexA.neighbors
+
+
+def test_add_vertex_neighbors(vertexA):
+    # Arrange
+    vertexB = Vertex("B")
+    vertexC = Vertex("C")
+
+    # Act
+    vertexA.add_neighbor(vertexB)
+    vertexA.add_neighbor(vertexC)
+
+    # Assert
+    assert len(vertexA.neighbors) == 2
+    assert vertexB in vertexA.neighbors
+    assert vertexC in vertexA.neighbors
